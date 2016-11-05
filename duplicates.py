455e80d660b9f50d7ad00f4f6c7d3cb30d6a6938
Alexey <beastrock@mail.ru>
2016-11-05 19:52:43 +0300

    Renaming values for easy code understanding

--- duplicates.py
+++ duplicates.py
@@ -1,34 +1,35 @@
 import os
 
 
-def get_duplicates_dictionaries_list(*paths):
+def get_duplicates_list(*paths):
     files = []  # the list structure is [file_path, (file_name, size), ...]
-    duplicates_in_folder = False
+    no_duplicates_in_folder = True
     for path in paths:
         for folder_name, sub_folders, file_names in os.walk(path):
             for file_name in file_names:
                 file_path = os.path.join(folder_name, file_name)
                 file_size = os.path.getsize(file_path)
                 if (file_name, file_size) in files:
+                    no_duplicates_in_folder = False
                     # dupl_path - path of the first from two duplicates, which
                     # is already in list. Named shortly for str length < 79.
+                    # file_path in if - path of the second from two duplicates
                     dupl_path = files[files.index((file_name, file_size)) - 1]
                     yield {file_name: (file_path, dupl_path)}
-                    duplicates_in_folder = True
                 files.append(file_path)
                 files.append((file_name, file_size))
-    if not duplicates_in_folder:
+    if no_duplicates_in_folder:
         yield None
 
 
-def print_duplicates(duplicates_dictionaries_list):
+def print_duplicates(duplicates_list):
     of_the_first_file = 0
     of_the_second_file = 1
-    for duplicates_dictionary in duplicates_dictionaries_list:
-        if not duplicates_dictionary:
+    for duplicates_dictionaries in duplicates_list:
+        if not duplicates_dictionaries:
             print("Duplicates have not been found")
             return
-        for name, path in duplicates_dictionary.items():
+        for name, path in duplicates_dictionaries.items():
             print("\nDuplicates of {} have been found in:\n{}\n{}".format
                   (name, path[of_the_first_file], path[of_the_second_file]))
     return
@@ -38,6 +39,5 @@ if __name__ == "__main__":
     folder_path = input("Input desired folder path:\n")
     optional_folder_path = input(
         "Input another folder path or type enter to continue:\n")
-    duplicates_dictionary_list = get_duplicates_dictionaries_list(
-        folder_path, optional_folder_path)
-    print_duplicates(duplicates_dictionary_list)
+    duplicates_list = get_duplicates_list(folder_path, optional_folder_path)
+    print_duplicates(duplicates_list)
