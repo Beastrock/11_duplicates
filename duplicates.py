2f2c3f53039fa235c0a8916f5542436b29c97b0d
Alexey <beastrock@mail.ru>
2016-11-23 20:23:17 +0300

    Change code constructions [Илья, я собой доволен =)]
    Separate get_duplicates_list on two: get_all_files_info and get_duplicates. Use list comprehensions to short the code. Now script returns two or more duplicates at once, NOT IN PAIRS like it was.

--- duplicates.py
+++ duplicates.py
@@ -1,46 +1,36 @@
 import os
 
 
-def get_duplicates_list(*paths):
-    """
-    The files list structure is [file_path, (file_name, size), ...]
-    dupl_path - path of the first from two duplicates, which is already
-    in list. Named shortly because of pep8 string length < 79 symbols.
-    file_path in if block - path of the second from two duplicates.
-    """
-    files = []
-    no_duplicates_in_folder = True
-    for path in paths:
-        for folder_name, sub_folders, file_names in os.walk(path):
-            for file_name in file_names:
-                file_path = os.path.join(folder_name, file_name)
-                file_size = os.path.getsize(file_path)
-                if (file_name, file_size) in files:
-                    no_duplicates_in_folder = False
-                    dupl_path = files[files.index((file_name, file_size)) - 1]
-                    yield {file_name: (file_path, dupl_path)}
-                files.append(file_path)
-                files.append((file_name, file_size))
-    if no_duplicates_in_folder:
-        yield None
+def get_all_files_info(*paths):
+    return [(name, os.path.getsize(os.path.join(folder, name)), os.path.join(folder, name))
+            for path in paths
+            for folder, sub_folders, file_names in os.walk(path)
+            for name in file_names]
 
 
-def print_duplicates(duplicates_list):
-    of_the_first_file = 0
-    of_the_second_file = 1
-    for duplicates_dictionaries in duplicates_list:
-        if not duplicates_dictionaries:
-            print("Duplicates have not been found")
-            return
-        for name, path in duplicates_dictionaries.items():
-            print("\nDuplicates of {} have been found in:\n{}\n{}".format
-                  (name, path[of_the_first_file], path[of_the_second_file]))
-    return
+def get_duplicates(files_info):
+    file_name_and_file_path_dict = {}
+    for file_name, file_size, file_path in files_info:
+        file_name_and_file_path_dict.setdefault(file_name, [])
+        file_name_and_file_path_dict[file_name].append(file_path)
+    for file_name, file_paths in file_name_and_file_path_dict.items():
+        if len(file_paths) < 1:
+            del [file_name]
+    return file_name_and_file_path_dict
+
+
+def print_duplicates(duplicates_dict):
+    if not duplicates_dict:
+        print("Duplicates have not been found")
+        return
+    for duplicate_name, duplicates_paths in duplicates_dict.items():
+        print("\nDuplicates of {} have been found in:".format(duplicate_name))
+        for duplicates_path in duplicates_paths:
+            print("{}".format(duplicates_path))
 
 
 if __name__ == "__main__":
-    folder_path = input("Input desired folder path:\n")
-    optional_folder_path = input(
-        "Input another folder path or press ENTER to continue:\n")
-    duplicates_list = get_duplicates_list(folder_path, optional_folder_path)
-    print_duplicates(duplicates_list)
+    folder = input("Input desired folder path:\n")
+    optional_folder = input("Input one more folder path or press ENTER to continue:\n")
+    all_files = get_all_files_info(folder, optional_folder)
+    print_duplicates(get_duplicates(all_files))
