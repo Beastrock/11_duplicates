03e21f546f8899ad45fcdd56e81fc5e02c01088c
Alexey <beastrock@mail.ru>
2016-11-24 22:54:06 +0300

    Change constructions of functions
    наконец-то понял как все вытащить. всего лишь надо было сделать словарь с ключом "имя:размер_файла", а дальше уже легко

--- duplicates.py
+++ duplicates.py
@@ -1,36 +1,35 @@
 import os
 
 
-def get_all_files_info(*paths):
-    return [(name, os.path.getsize(os.path.join(folder, name)), os.path.join(folder, name))
-            for path in paths
-            for folder, sub_folders, file_names in os.walk(path)
-            for name in file_names]
+def get_duplicates(*paths):
+    files_info = {}
+    for path in paths:
+        for folder_name, sub_folders, file_names in os.walk(path):
+            for file_name in file_names:
+                file_path = os.path.join(folder_name, file_name)
+                file_size = os.path.getsize(os.path.join(folder_name, file_name))
+                file_name_and_size = "{}:{}".format(file_name, file_size)
+                files_info.setdefault(file_name_and_size, [])
+                files_info[file_name_and_size].append(file_path)
+    if not [value for value in files_info.values() if len(value) > 1]:
+        return None
+    else:
+        return {key: value for key, value in files_info.items() if len(value) > 1}
 
 
-def get_duplicates(files_info):
-    file_name_and_file_path_dict = {}
-    for file_name, file_size, file_path in files_info:
-        file_name_and_file_path_dict.setdefault(file_name, [])
-        file_name_and_file_path_dict[file_name].append(file_path)
-    for file_name, file_paths in file_name_and_file_path_dict.items():
-        if len(file_paths) < 1:
-            del [file_name]
-    return file_name_and_file_path_dict
-
-
-def print_duplicates(duplicates_dict):
-    if not duplicates_dict:
+def print_duplicates(duplicates):
+    if not duplicates:
         print("Duplicates have not been found")
         return
-    for duplicate_name, duplicates_paths in duplicates_dict.items():
-        print("\nDuplicates of {} have been found in:".format(duplicate_name))
-        for duplicates_path in duplicates_paths:
-            print("{}".format(duplicates_path))
+    for file_name_and_size, file_paths in duplicates.items():
+        file_name = file_name_and_size.split(":")[0]
+        print("\nDuplicates of \"{}\" have been found in:".format(file_name))
+        for file_path in file_paths:
+            print(file_path)
 
 
 if __name__ == "__main__":
     folder = input("Input desired folder path:\n")
     optional_folder = input("Input one more folder path or press ENTER to continue:\n")
-    all_files = get_all_files_info(folder, optional_folder)
-    print_duplicates(get_duplicates(all_files))
+    all_files = get_duplicates(folder, optional_folder)
+    print_duplicates(all_files)
